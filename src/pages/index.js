3774a2f1b9bd625df3ef4c8afad0c24573963b9a
knzn <rdacles@gmail.com>
2020-01-04 23:55:00 +0800

    Adding About section and Blog page

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,16 +27,31 @@ const Index = () => {
           </ul>
         </nav>
       </header>
-      <div className="main-page">
+      <section className="main-page">
         <h4>Hi, my name is</h4>
         <h1>Roberto Dacles.</h1>
-        <h1>I create website with passion</h1>
+        <h1>A Fullstack Developer</h1>
         <p>
           I'm a self taught web developer based in Philippines,
           <br /> building high-quality websites and applications
         </p>
         <button>Get in Touch</button>
-      </div>
+      </section>
+      <section id="about">
+        <p>
+          <span>01.</span> Hi, my name is{" "}
+          <em>
+            <strong>Roberto Dacles</strong>
+          </em>
+          , From Philippines. I am a Internet
+          <br />
+          Cafe Owner for almost 6 years. I started coding around 2018, <br />
+          and took it seriously last year. I dont have any college <br />
+          degree but my passion and dedication on this career <br /> will help
+          me to be come successful.
+          <br />
+        </p>
+      </section>
     </div>
   )
 }
